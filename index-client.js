dbaa0abfa2b4ed45ae499c39ed81e1d1afa8231f
Kozhin Vladimir <affka@affka.ru>
2015-06-14 08:27:22 +0700

    client version, fix deps

---
+++ index-client.js
@@ -0,0 +1,23 @@
+
+'use strict';
+
+require('jii');
+
+// Load framework files
+require('./lib/base/ActiveRecord');
+require('./lib/base/Model');
+require('./lib/validators/BooleanValidator');
+require('./lib/validators/CompareValidator');
+require('./lib/validators/DateValidator');
+require('./lib/validators/DefaultValueValidator');
+require('./lib/validators/EmailValidator');
+require('./lib/validators/FilterValidator');
+require('./lib/validators/InlineValidator');
+require('./lib/validators/NumberValidator');
+require('./lib/validators/RangeValidator');
+require('./lib/validators/RegularExpressionValidator');
+require('./lib/validators/RequiredValidator');
+require('./lib/validators/SafeValidator');
+require('./lib/validators/StringValidator');
+require('./lib/validators/UrlValidator');
+require('./lib/validators/Validator');
